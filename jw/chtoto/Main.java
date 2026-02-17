6b37bf3ae6d803c7426fcfb72b7ae582ab1d8d4c
Пан Роман <@step.kz>
2026-02-17 10:08:05 +0500

    egregeg

---
+++ jw/chtoto/Main.java
@@ -0,0 +1,30 @@
+// package jw;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int[][] matrix = new int[3][4];
+
+//         int counter = 1;
+//         for (int i = 0; i < matrix.length; i++) {
+//             for (int j = 0; j < matrix[i].length; j++) {
+//                 System.out.print(" "+ i +""+j + " ");
+//                 matrix[i][j] = counter++;
+                
+//             }
+            
+//         }
+
+
+//         int sum = 0;
+//         int sum2 = 0;
+//         for (int i = 0; i < matrix.length; i++){
+//             for (int j = 0; j < matrix.length; j++){
+//                 sum += matrix[i][j]
+//             }
+//             sum2 += sum;
+            
+            
+//         }
+//         System.out.println(sum);
+//     }
+// }
